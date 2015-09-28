dee44b3728b68bd8f4295ad113115cd242b43eed
JorgeRoldan <jorge.roldan.contractor@dev.bbva.com>
2015-09-28 10:55:49 +0200

    Use Jackson to generate output Json

--- google-app-engine-guice-jersey-default/src/main/java/com/roldan/google/interfaces/jersey/resources/HelloWorldResource.java
+++ google-app-engine-guice-jersey-default/src/main/java/com/roldan/google/interfaces/jersey/resources/HelloWorldResource.java
@@ -4,8 +4,8 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
-import com.google.gson.Gson;
 import com.roldan.google.interfaces.guice.Service;
 
 @Path("helloworld")
@@ -17,9 +17,8 @@ public class HelloWorldResource {
 	
 	@GET
 	@Produces("application/json")
-	public String getHello() {
+	public Response getHello() {
 		Greeting greeting = new Greeting(service.getGreeting(), "Jorge");
-		Gson gson = new Gson();
-		return gson.toJson(greeting); 
+		return Response.status(Response.Status.OK).entity(greeting).build(); 
 	}
 }
